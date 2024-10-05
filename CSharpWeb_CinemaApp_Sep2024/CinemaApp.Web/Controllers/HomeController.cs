3ab0ff38ba919970496d77921b13d3dd332536ea
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-10-05 22:34:05 +0300

    Add files via upload

---
+++ CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using CinemaApp.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace CinemaApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
