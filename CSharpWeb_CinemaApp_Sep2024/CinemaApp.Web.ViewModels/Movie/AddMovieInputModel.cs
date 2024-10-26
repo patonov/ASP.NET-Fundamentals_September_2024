2d5315f5f8168e7705e6d6333b7490dbc708a466
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-10-26 22:27:09 +0300

    Add files via upload

--- CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web.ViewModels/Movie/AddMovieInputModel.cs
+++ CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web.ViewModels/Movie/AddMovieInputModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,27 @@ namespace CinemaApp.Web.ViewModels.Movie
             this.ReleaseDate = DateTime.UtcNow.ToString("MM/yyyy");
         }
 
+        [Required]
+        [MaxLength(40)]
         public string Title { get; set; } = null!;
 
+        [Required]
+        [MaxLength(12)]
         public string Genre { get; set; } = null!;
 
+        [Required]
         public string ReleaseDate { get; set; }
 
+        [Required]
+        [MaxLength(25)]
         public string Director { get; set; } = null!;
         
+        [Required]
+        [Range(0,240)]
         public int Duration { get; set; }
 
+        [Required]
+        [MaxLength(250)]
         public string Description { get; set; } = null!;
 
 
