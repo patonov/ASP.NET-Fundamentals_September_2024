e63155610e39d7858087afe4591e435648e87434
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-11-16 20:13:30 +0200

    Add files via upload

---
+++ CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web/Controllers/CinemaController.cs
@@ -0,0 +1,34 @@
+﻿using CinemaApp.Web.Data;
+using CinemaApp.Web.Models;
+using CinemaApp.Web.ViewModels.Cinema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly CinemaDbContext cinemaDbContext;
+
+        public CinemaController(CinemaDbContext cinemaDbContext)
+        { 
+            this.cinemaDbContext = cinemaDbContext;
+        }
+
+        public IActionResult Index()
+        { 
+            IEnumerable<CinemaIndexViewModel> cinemas = this.cinemaDbContext
+                .Cinemas
+                .Select(c => new CinemaIndexViewModel() 
+                { 
+                    Id = c.Id.ToString(),
+                    Name = c.Name,
+                    Location = c.Location,
+                })
+                .ToArray();
+
+            return View(cinemas);
+        }
+
+
+    }
+}
