8a87ea7fb7fdfd4c7091a308d84060d09a9d53b5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-11-23 23:01:23 +0200

    Add files via upload

--- CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web.ViewModels/Cinema/AddCinemaFormModel.cs
+++ CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web.ViewModels/Cinema/AddCinemaFormModel.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CinemaApp.Web.ViewModels.Cinema
 {
+    using static Common.EntityValidationConstants.Cinema;
+
     public class AddCinemaFormModel
     {
+        [Required]
+        [MinLength(CinemaNameMinLength)]
+        [MaxLength(CinemaNameMaxLength)]
         public string Name { get; set; } = null!;
          
+        [Required]
         public string Location { get; set; } = null!;
     }
 }
