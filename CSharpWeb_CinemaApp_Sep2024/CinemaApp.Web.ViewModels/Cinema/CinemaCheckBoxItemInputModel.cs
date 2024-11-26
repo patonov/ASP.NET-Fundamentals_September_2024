92a7a147bfd574a9f9f03787f3809bf17d0eb8e5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-11-26 23:23:37 +0200

    Add files via upload

---
+++ CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web.ViewModels/Cinema/CinemaCheckBoxItemInputModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Common.EntityValidationConstants.Cinema;
+
+namespace CinemaApp.Web.ViewModels.Cinema
+{
+    public class CinemaCheckBoxItemInputModel
+    {
+        [Required]
+        public string Id { get; set; } = null!;
+
+        [Required]
+        [MaxLength(CinemaNameMaxLength)]
+        [MinLength(CinemaNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ CSharpWeb_CinemaApp_Sep2024/CinemaApp.Web.ViewModels/Movie/AddMovieToCinemaInputModel.cs
@@ -0,0 +1,24 @@
+﻿using CinemaApp.Web.ViewModels.Cinema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Common.EntityValidationConstants.Movie;
+
+namespace CinemaApp.Web.ViewModels.Movie
+{
+    public class AddMovieToCinemaInputModel
+    {
+        [Required]
+        public string Id { get; set; } = null!;
+
+        [Required]
+        [MaxLength(MovieTitleMaxLength)]
+        public string MovieTitle { get; set; } = null!;
+
+        public IEnumerable<CinemaCheckBoxItemInputModel> Cinemas { get; set; } = new HashSet<CinemaCheckBoxItemInputModel>();
+    }
+}
