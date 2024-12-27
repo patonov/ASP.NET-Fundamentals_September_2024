bc5b63c29c399612565dba6392261c59961ca808
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2024-12-27 22:29:55 +0200

    Add files via upload

--- GameZone/GameZone/Models/GameInfoViewModel.cs
+++ GameZone/GameZone/Models/GameInfoViewModel.cs
@@ -2,6 +2,16 @@
 {
     public class GameInfoViewModel
     {
+        public int Id { get; set; }
 
+        public required string Title { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public required string Genre { get; set; }
+        
+        public required string ReleasedOn { get; set; }
+        
+        public required string Publisher { get; set; } 
     }
 }
