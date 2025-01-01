a84a44380b0060652fa49121222df4b5a143e49a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-01-01 22:23:18 +0200

    Add files via upload

--- GameZone/GameZone/Models/GameViewModel.cs
+++ GameZone/GameZone/Models/GameViewModel.cs
@@ -1,7 +1,27 @@
-﻿namespace GameZone.Models
+﻿using GameZone.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.Models
 {
     public class GameViewModel
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
 
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 3)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ReleasedOn { get; set; } = DateTime.Today.ToString("yyyy-MM-dd"); 
+
+        [Required]
+        public int GenreId { get; set; }
+
+        public List<Genre> Genres { get; set; } = new List<Genre>();
     }
 }
